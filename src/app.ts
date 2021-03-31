b715dbc04f9feecf463112fa9de4ff8349bad77f
SeongBong <chopisal@gmail.com>
2021-03-31 16:25:33 +0900

    motion

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,81 @@
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent, PageItemComponent } from "./components/page/page.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
+import { Component } from "./components/components.js";
+import { InputDialog, MediaData, TextData } from "./components/dialog/dialog.js";
+import { MediaSectionInput } from "./components/dialog/input/media-input.js";
+import { TextSectionInput } from "./components/dialog/input/text-input.js";
+
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+  new (): T;
+};
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
 
-  constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
-    const image = new ImageComponent(
-      "Image Title",
-      "https://picsum.photos/600/400"
+    this.bindElementToDialog<MediaSectionInput>(
+      "#new-image",
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
     );
-    image.attachTo(appRoot, "beforeend");
 
-    const note = new NoteComponent("Note Title", "Note Body");
-    note.attachTo(appRoot, "beforeend");
+    this.bindElementToDialog<MediaSectionInput>(
+      "#new-video",
+      MediaSectionInput,
+      (input: MediaSectionInput) => new VideoComponent(input.title, input.url)
+    );
 
-    const todo = new TodoComponent("Todo Title", "Todo Item");
-    todo.attachTo(appRoot, "beforeend");
+    this.bindElementToDialog<TextSectionInput>(
+      "#new-note",
+      TextSectionInput,
+      (input: TextSectionInput) => new NoteComponent(input.title, input.body)
+    );
+
+    this.bindElementToDialog<TextSectionInput>(
+      "#new-todo",
+      TextSectionInput,
+      (input: TextSectionInput) => new TodoComponent(input.title, input.body)
+    );
+
+    // For demo :)
+    this.page.addChild(new ImageComponent("Image Title", "https://picsum.photos/800/400"));
+    this.page.addChild(new VideoComponent("Video Title", "https://youtu.be/D7cwvvA7cP0"));
+    this.page.addChild(new NoteComponent("Note Title", "Don't forget to code your dream"));
+    this.page.addChild(new TodoComponent("Todo Title", "TypeScript Course!"));
+    this.page.addChild(new ImageComponent("Image Title", "https://picsum.photos/800/400"));
+    this.page.addChild(new VideoComponent("Video Title", "https://youtu.be/D7cwvvA7cP0"));
+    this.page.addChild(new NoteComponent("Note Title", "Don't forget to code your dream"));
+    this.page.addChild(new TodoComponent("Todo Title", "TypeScript Course!"));
+  }
+
+  private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+    selector: string,
+    InputComponent: InputComponentConstructor<T>,
+    makeSection: (input: T) => Component
+  ) {
+    const element = document.querySelector(selector)! as HTMLButtonElement;
+    element.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      const input = new InputComponent();
+      dialog.addChild(input);
+      dialog.attachTo(this.dialogRoot);
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(this.dialogRoot);
+      });
+      dialog.setOnSubmitListener(() => {
+        const image = makeSection(input);
+        this.page.addChild(image);
+        dialog.removeFrom(this.dialogRoot);
+      });
+    });
   }
 }
 
-new App(document.querySelector(".document")! as HTMLElement);
+new App(document.querySelector(".document")! as HTMLElement, document.body);
--- src/components/page/item/note.ts
+++ src/components/page/item/note.ts
@@ -4,17 +4,14 @@ export class NoteComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, body: string) {
     super(`<section class="note">
             <h2 class="note__title"></h2>
+            <h2 class="page-item__title note__title"></h2>  
             <p class="note__body"></p>
            </section>`);
 
-    const titleElement = this.element.querySelector(
-      ".note__title"
-    )! as HTMLHeadElement;
+    const titleElement = this.element.querySelector(".note__title")! as HTMLHeadElement;
     titleElement.textContent = title;
 
-    const noteElement = this.element.querySelector(
-      ".note__body"
-    )! as HTMLParagraphElement;
+    const noteElement = this.element.querySelector(".note__body")! as HTMLParagraphElement;
     noteElement.textContent = body;
   }
 }
